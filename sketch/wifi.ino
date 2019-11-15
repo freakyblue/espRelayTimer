ea0a8041d521d0f5df0f3f686a11fe8de949137d
jonny <github@jonny-seitz.de>
2019-11-15 09:22:55 +0100

    Add wifi.ino

---
+++ sketch/wifi.ino
@@ -0,0 +1,17 @@
+const char* ssid = "";
+const char* password = "";
+
+void initWifi() {
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address:");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+}
