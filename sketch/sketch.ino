739ecc9b10c81392d287e80f00e09629a99cdf18
jonny <github@jonny-seitz.de>
2019-11-15 08:57:16 +0100

    Add output gpio defintion & serial output

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,5 +1,15 @@
+#include <ESP8266WiFi.h>
+
+//output pin
+const int gpioOut = 4;
+
 void setup() {
+  Serial.begin(115200);
+  delay(300);
+  Serial.println("start");
 }
 
 void loop() {
+  delay(1000);
+  Serial.println("run");
 }
