1be73d47d383de4943633db4a619220b27ec23f9
jonny <github@jonny-seitz.de>
2019-11-15 09:20:55 +0100

    Init wifi connection

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -5,11 +5,10 @@ const int gpioOut = 4;
 
 void setup() {
   Serial.begin(115200);
-  delay(300);
+  delay(30);
+  initWifi();
   Serial.println("start");
 }
 
 void loop() {
-  delay(1000);
-  Serial.println("run");
 }
